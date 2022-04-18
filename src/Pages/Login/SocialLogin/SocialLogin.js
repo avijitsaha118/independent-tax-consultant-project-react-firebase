daab926e0c7560c33d2783f90fe120ee7d48f744
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-18 21:50:22 +0600

    login updated

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import googleLogo from '../../../Images/Logo/google.png';
-// import fbLogo from '../../../Images/social/facebook-logo.jpg';
-// import gitLogo from '../../../Images/social/github-logo.png';
+import gitLogo from '../../../Images/Logo/github.png';
 import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
@@ -13,7 +12,7 @@ const SocialLogin = () => {
     const navigate = useNavigate();
     let errorElement;
 
-    if(loading || loading1){
+    if (loading || loading1) {
         return <Loading></Loading>
     }
 
@@ -44,12 +43,9 @@ const SocialLogin = () => {
                     <img style={{ width: '30px' }} src={googleLogo} alt='logo-img'></img>
                     <span className='px-2'> Google Sign In</span>
                 </button>
-                <button className='btn btn-info w-50 d-block mx-auto my-2'>
-                    {/* <img style={{ width: '30px' }} src={fbLogo} alt='logo-img'></img> */}
-                    <span className='px-2'> Facebook Sign In</span>
-                </button>
+
                 <button onClick={() => signInWithGithub()} className='btn btn-info w-50 d-block mx-auto'>
-                    {/* <img style={{ width: '30px' }} src={gitLogo} alt='logo-img'></img> */}
+                    <img style={{ width: '30px' }} src={gitLogo} alt='logo-img'></img>
                     <span className='px-2'> GitHub Sign In</span>
                 </button>
             </div>
