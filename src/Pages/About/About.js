e1bd65a32f935c368703614904d563d497d9e578
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-18 22:56:51 +0600

    about me added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,10 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-           <h1>This is about</h1> 
+           <h1>About Me</h1> 
+
+           <h4>Name: Avijit Saha</h4>
+           <p>Goal: From now to next Four or Five Month I discover myself as a Web Developer. From 31 december, 2021 I started a journey with PHero. <br/> I enjoyed it and this course totaly changed me. Now I can know how to work hard, how to maintain time, how to be disciplined.</p>
         </div>
     );
 };
--- src/Pages/Home/Features/Features.js
+++ src/Pages/Home/Features/Features.js
@@ -6,14 +6,14 @@ import Feature from '../Feature/Feature';
 
 const Features = () => {
     const features = [
-        { id: 1, name: 'For Doctor', img: doctor },
-        { id: 2, name: 'For Teacher', img: teacher },
-        { id: 3, name: 'For Web Developer', img: developer }
+        { id: 1, name: 'For Doctor', description: 'It is my honourable respect to Doctor, Dentist and Veterinarian. You can complete your tax return complience from me now at a cheap price.', img: doctor },
+        { id: 2, name: 'For Teacher', description:'Teaching is a very noble profession that shapes the character, caliber, and future of an individual. My special honor for the people who come from this profession.',  img: teacher },
+        { id: 3, name: 'For Web Developer', description:'Hey, Web or Software developer! You can Log in with your GitHub account and take special price offer from me to fill your indivitual tax return.', img: developer }
     ]
     return (
         <div className='container'>
                         <h2 className='text-primary text-center mt-3'>Special Features</h2>
-                        <p>This year I declare special discount offer for the Doctors, Teachers and Web developers</p>
+                        <p style={{fontSize:"20px"}}>This year I declare a special discount offer on Individual Tax Return for the Doctors, Teachers and Web developers.</p>
             <div className='row'>
                 {
                     features.map(feature => <Feature
