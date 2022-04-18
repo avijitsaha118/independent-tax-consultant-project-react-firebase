f94f4d09abf89fd2661ca218e9f6b29ee84bcc66
avijitsaha118 <sahaavijit908@gmail.com>
2022-04-18 23:58:09 +0600

    final commit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,7 +6,8 @@ const About = () => {
            <h1>About Me</h1> 
 
            <h4>Name: Avijit Saha</h4>
-           <p>Goal: From now to next Four or Five Month I discover myself as a Web Developer. From 31 december, 2021 I started a journey with PHero. <br/> I enjoyed it and this course totaly changed me. Now I can know how to work hard, how to maintain time, how to be disciplined.</p>
+           <p>Goal: From now to next Four or Five Month I discover myself as a Web Developer. From 31 december, 2021 I started a journey with PHero. <br/> I enjoyed it and this course totaly changed me. Now I can know how to work hard, how to maintain time, how to be disciplined.
+           Besides this i also read and learn how to read doccumentation. As a developer reading doccumentation is very important. I love this journey with Phero.</p>
         </div>
     );
 };
